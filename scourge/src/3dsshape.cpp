e2fc381d6bdc4c40de3494181d67886e28bb0376
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-02-01 06:49:50 +0000

    started to work on 3ds loader. Still broken... need to fix image locations, size, position and color

---
+++ scourge/src/3dsshape.cpp
@@ -0,0 +1,211 @@
+/***************************************************************************
+                          3dsshape.cpp  -  description
+                             -------------------
+    begin                : Fri Oct 3 2003
+    copyright            : (C) 2003 by Gabor Torok
+    email                : cctorok@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+  
+#include "3dsshape.h"
+
+C3DSShape::C3DSShape(char *file_name, float div,
+                   GLuint texture[],
+                   int width, int depth, int height,
+                   char *name,
+                   Uint32 color, GLuint display_list, Uint8 shapePalIndex) :
+  // passing 0 for texture causes glshape to not init
+  GLShape(0, width, depth, height, name, color, display_list, shapePalIndex) {
+  commonInit(file_name, div);    
+}
+
+C3DSShape::C3DSShape(char *file_name, float div,
+                   GLuint texture[],
+                   int width, int depth, int height,
+                   char *name, char **description, int descriptionCount,
+                   Uint32 color, GLuint display_list, Uint8 shapePalIndex) :
+  // passing 0 for texture causes glshape to not init
+  GLShape(0, width, depth, height, name, description, descriptionCount, color, display_list, shapePalIndex) {
+  commonInit(file_name, div);    
+}
+
+C3DSShape::~C3DSShape() {
+  // When we are done, we need to free all the model data
+  // We do this by walking through all the objects and freeing their information
+
+  // Go through all the objects in the scene
+  for(int i = 0; i < g_3DModel.numOfObjects; i++) {
+	// Free the faces, normals, vertices, and texture coordinates.
+	delete [] g_3DModel.pObject[i].pFaces;
+	delete [] g_3DModel.pObject[i].pNormals;
+	delete [] g_3DModel.pObject[i].pVerts;
+	delete [] g_3DModel.pObject[i].pTexVerts;
+  }
+}
+
+void C3DSShape::commonInit(char *file_name, float div) {
+  g_Texture[0] = 0;
+  g_ViewMode = GL_TRIANGLES;
+  this->div = div;
+
+  // First we need to actually load the .3DS file.  We just pass in an address to
+  // our t3DModel structure and the file name string we want to load ("face.3ds").
+  g_Load3ds.Import3DS(&g_3DModel, file_name);         // Load our .3DS file into our model structure
+
+  // Depending on how many textures we found, load each one (Assuming .BMP)
+  // If you want to load other files than bitmaps, you will need to adjust CreateTexture().
+  // Below, we go through all of the materials and check if they have a texture map to load.
+  // Otherwise, the material just holds the color information and we don't need to load a texture.
+
+  // Go through all the materials
+  for(int i = 0; i < g_3DModel.numOfMaterials; i++) {
+	// Check to see if there is a file name to load in this material
+	if(strlen(g_3DModel.pMaterials[i].strFile) > 0) {
+	  // Use the name of the texture file to load the bitmap, with a texture ID (i).
+	  // We pass in our global texture array, the name of the texture, and an ID to reference it. 
+	  fprintf(stderr, "Loading texture: %s\n", g_3DModel.pMaterials[i].strFile);
+	  CreateTexture((GLuint*)g_Texture, g_3DModel.pMaterials[i].strFile, i);           
+	}
+	// Set the texture ID for this material
+	g_3DModel.pMaterials[i].texureId = i;
+  }
+  
+  // Find the lowest point
+  float minx, miny, minz;  
+  float maxx, maxy, maxz;
+  minx = miny = minz = maxx = maxy = maxz = 0;
+  if(g_3DModel.pObject.size() > 0) {
+    // Get the current object that we are displaying
+    t3DObject *pObject = &g_3DModel.pObject[0];  
+    for(int j = 0; j < pObject->numOfFaces; j++) {
+      for(int whichVertex = 0; whichVertex < 3; whichVertex++) {
+        int index = pObject->pFaces[j].vertIndex[whichVertex];
+        if(pObject->pVerts[ index ].x > maxx) maxx = pObject->pVerts[ index ].x;
+        if(pObject->pVerts[ index ].y > maxy) maxy = pObject->pVerts[ index ].y;
+        if(pObject->pVerts[ index ].z > maxz) maxz = pObject->pVerts[ index ].z;
+        if(pObject->pVerts[ index ].x < minx) minx = pObject->pVerts[ index ].x;
+        if(pObject->pVerts[ index ].y < miny) miny = pObject->pVerts[ index ].y;
+        if(pObject->pVerts[ index ].z < minz) minz = pObject->pVerts[ index ].z;        
+      }
+    }
+  }
+  movex = maxx - minx;
+  movey = maxy - miny;
+  movez = maxz - minz;
+}
+
+void C3DSShape::draw() {
+
+
+
+  //FIXME: fix size/position and color (why blue?)
+  //  GLShape::draw();
+
+  glPushMatrix();
+  glScalef( div, div, div );
+  glTranslatef( -movex, movey, movez );
+
+  // I am going to attempt to explain what is going on below up here as not to clutter the 
+  // code below.  We have a model that has a certain amount of objects and textures.  We want 
+  // to go through each object in the model, bind it's texture map to it, then render it.
+  // To render the current object, we go through all of it's faces (Polygons).  
+  // What is a face you ask?  A face is just (in this case) a triangle of the object.
+  // For instance, a cube has 12 faces because each side has 2 triangles.
+  // You might be thinking.  Well, if there are 12 faces in a cube, that makes
+  // 36 vertices that we needed to read in for that object.  Not really true.  Because
+  // a lot of the vertices are the same, since they share sides, they only need to save
+  // 8 vertices, and ignore the duplicates.  Then, you have an array of all the
+  // unique vertices in that object.  No 2 vertices will be the same.  This cuts down
+  // on memory.  Then, another array is saved, which is the index numbers for each face,
+  // which index in to that array of vertices.  That might sound silly, but it is better
+  // than saving tons of duplicate vertices.  The same thing happens for UV coordinates.
+  // You don't save duplicate UV coordinates, you just save the unique ones, then an array
+  // that index's into them.  This might be confusing, but most 3D files use this format.
+  // This loop below will stay the same for most file formats that you load, so all you need
+  // to change is the loading code.  You don't need to change this loop (Except for animation).
+  
+  // Since we know how many objects our model has, go through each of them.
+  for(int i = 0; i < g_3DModel.numOfObjects; i++) {
+	// Make sure we have valid objects just in case. (size() is in the vector class)
+	if(g_3DModel.pObject.size() <= 0) break;
+
+	// Get the current object that we are displaying
+	t3DObject *pObject = &g_3DModel.pObject[i];
+	
+	// Reset the color to normal again
+	//glColor3ub(255, 255, 255);
+
+	// Check to see if this object has a texture map, if so bind the texture to it.
+	if(pObject->bHasTexture) {
+	  // Bind the texture map to the object by it's materialID
+	  glBindTexture(GL_TEXTURE_2D, g_Texture[pObject->materialID]);
+	}
+	
+	// This determines if we are in wireframe or normal mode
+	glBegin(g_ViewMode);                    // Begin drawing with our selected mode (triangles or lines)
+	
+	// Go through all of the faces (polygons) of the object and draw them
+	for(int j = 0; j < pObject->numOfFaces; j++) {
+	  // Go through each corner of the triangle and draw it.
+	  for(int whichVertex = 0; whichVertex < 3; whichVertex++) {
+		// Get the index for each point of the face
+		int index = pObject->pFaces[j].vertIndex[whichVertex];
+		
+		// Give OpenGL the normal for this vertex.
+		glNormal3f(pObject->pNormals[ index ].x, pObject->pNormals[ index ].y, pObject->pNormals[ index ].z);
+		
+		// If the object has a texture associated with it, give it a texture coordinate.
+		if(pObject->bHasTexture) {
+		  
+		  // Make sure there was a UVW map applied to the object or else it won't have tex coords.
+		  if(pObject->pTexVerts) {
+			glTexCoord2f(pObject->pTexVerts[ index ].x, pObject->pTexVerts[ index ].y);
+		  }
+		} else {
+		  
+		  // Make sure there is a valid material/color assigned to this object.
+		  // You should always at least assign a material color to an object, 
+		  // but just in case we want to check the size of the material list.
+		  // if the size is at least one, and the material ID != -1,
+		  // then we have a valid material.
+		  if(g_3DModel.pMaterials.size() && pObject->materialID >= 0) {
+			// Get and set the color that the object is, since it must not have a texture
+			BYTE *pColor = g_3DModel.pMaterials[pObject->materialID].color;
+			
+			// Assign the current color to this model
+			//glColor3ub(pColor[0], pColor[1], pColor[2]);
+		  }
+		}
+		
+		// Pass in the current vertex of the object (Corner of current face)
+		glVertex3f(pObject->pVerts[ index ].x, pObject->pVerts[ index ].y, pObject->pVerts[ index ].z);
+	  }
+	}
+	
+	glEnd();                                // End the drawing
+  }
+  glPopMatrix();
+}
+
+void C3DSShape::setupBlending() { 
+  glBlendFunc(GL_ONE, GL_ONE); 
+}
+
+void C3DSShape::endBlending() { 
+}
+
+bool C3DSShape::drawFirst() { 
+  return true; 
+}
+
+bool C3DSShape::drawLater() { 
+  return false; 
+}
--- scourge/src/Md2.h
+++ scourge/src/Md2.h
@@ -1,104 +1,20 @@
 #ifndef _MD2_H
 #define _MD2_H
 
+#include "constants.h"
+
 /**
  * Credit for this code is mainly due to:
  * DigiBen     digiben@gametutorials.com
  * Look up his other great tutorials at:
  * http://www.gametutorials.com
  */
-typedef unsigned char BYTE;
+
 using namespace std;
 
 /**
   *@author Gabor Torok
   */
-
-
-// This file includes all of the model structures that are needed to load
-// in a .Md2 file.  When it comes to skeletal animation, we need to add quite
-// a bit more variables to these structures.  Not all of the data will be used
-// because Quake2 models don't have such a need.  I decided to keep the structures
-// the same as the rest of the model loaders on our site so that we could eventually
-// use a base class in the future for a library.
-//
-
-#define MAX_TEXTURES 100								// The maximum amount of textures to load
-
-// This is our 3D point class.  This will be used to store the vertices of our model.
-class CVector3
-{
-public:
-	float x, y, z;
-};
-
-// This is our 2D point class.  This will be used to store the UV coordinates.
-class CVector2
-{
-public:
-	float x, y;
-};
-
-// This is our face structure.  This is is used for indexing into the vertex
-// and texture coordinate arrays.  From this information we know which vertices
-// from our vertex array go to which face, along with the correct texture coordinates.
-struct tFace
-{
-	int vertIndex[3];			// indicies for the verts that make up this triangle
-	int coordIndex[3];			// indicies for the tex coords to texture this face
-};
-
-// This holds the information for a material.  It may be a texture map of a color.
-// Some of these are not used, but I left them because you will want to eventually
-// read in the UV tile ratio and the UV tile offset for some models.
-struct tMaterialInfo
-{
-	char  strName[255];			// The texture name
-	char  strFile[255];			// The texture file name (If this is set it's a texture map)
-	BYTE  color[3];				// The color of the object (R, G, B)
-	int   texureId;				// the texture ID
-	float uTile;				// u tiling of texture  (Currently not used)
-	float vTile;				// v tiling of texture	(Currently not used)
-	float uOffset;			    // u offset of texture	(Currently not used)
-	float vOffset;				// v offset of texture	(Currently not used)
-} ;
-
-// This holds all the information for our model/scene.
-// You should eventually turn into a robust class that
-// has loading/drawing/querying functions like:
-// LoadModel(...); DrawObject(...); DrawModel(...); DestroyModel(...);
-struct t3DObject
-{
-	int  numOfVerts;			// The number of verts in the model
-	int  numOfFaces;			// The number of faces in the model
-	int  numTexVertex;			// The number of texture coordinates
-	int  materialID;			// The texture ID to use, which is the index into our texture array
-	bool bHasTexture;			// This is TRUE if there is a texture map for this object
-	char strName[255];			// The name of the object
-	CVector3  *pVerts;			// The object's vertices
-	CVector3  *pNormals;		// The object's normals
-	CVector2  *pTexVerts;		// The texture's UV coordinates
-	tFace *pFaces;				// The faces information of the object
-};
-
-// This holds our model information.  This should also turn into a robust class.
-// We use STL's (Standard Template Library) vector class to ease our link list burdens. :)
-struct t3DModel
-{
-	int numOfObjects;					// The number of objects in the model
-	int numOfMaterials;					// The number of materials for the model
-	vector<tMaterialInfo> pMaterials;	// The list of material information (Textures and colors)
-	vector<t3DObject> pObject;			// The object list for our model
-};
-
-
-
-
-
-
-
-typedef unsigned char byte;
-
 // These are the needed defines for the max values when loading .MD2 files
 #define MD2_MAX_TRIANGLES       4096
 #define MD2_MAX_VERTICES        2048
@@ -196,7 +112,7 @@ private:
     void ConvertDataStructures(t3DModel *pModel);
 
     // This computes the vertex normals for the object (used for lighting)
-    void ComputeNormals(t3DModel *pModel);
+    //void ComputeNormals(t3DModel *pModel);
 
     // This frees memory and closes the file
     void CleanUp();
