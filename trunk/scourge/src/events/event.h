b716ebe7fa6b73c23c5efe6a9739aaab60644bee
daroth-u <daroth-u@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-04-09 12:18:53 +0000

    Initial import

---
+++ trunk/scourge/src/events/event.h
@@ -0,0 +1,67 @@
+/***************************************************************************
+                          event.h  -  description
+                             -------------------
+    begin                : Thu Apr 8 2004
+    copyright            : (C) 2004 by Daroth-U
+    email                : daroth-u@ifrance.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef EVENT_H
+#define EVENT_H
+
+#include "../constants.h"
+#include "../date.h"
+
+
+/**
+  *@author Daroth-U
+  */
+ 
+class Date;  
+    
+class Event  {
+
+private:
+  Date eventDate;
+  Date timeOut; 
+  long nbExecutionsToDo;
+  long nbExecutions;
+  
+
+public:
+  static int id; // For debug purpose
+  
+  enum {
+    INFINITE_EXECUTIONS = -1  
+  }; 
+   
+  // This event will occur nbExecutionsTD time every tmOut from currentDate.  
+  Event(Date currentDate, Date tmOut, long nbExecutionsToDo);
+  
+  // This event will occur only one time at the given date
+  Event(Date eventDate);
+  
+  Event(); 
+  ~Event();
+  
+  virtual void execute()=0;
+  inline long getNbExecutionsToDo() { return nbExecutionsToDo; }
+  inline long getNbExecutions()     { return nbExecutions; }
+  inline void increaseNbExecutions(){ nbExecutionsToDo ++ ; }
+    
+  Date getEventDate() { return eventDate; }
+  Date getTimeOut()   { return timeOut;   }
+  void setEventDate(Date d) { eventDate = d; }  
+  
+};
+
+#endif
---
+++ trunk/scourge/src/events/thirsthungerevent.cpp
@@ -0,0 +1,96 @@
+/***************************************************************************
+                          thirsthungerevent.cpp  -  description
+                             -------------------
+    begin                : Thu Apr 8 2004
+    copyright            : (C) 2004 by Daroth-U
+    email                : daroth-u@ifrance.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "thirsthungerevent.h"
+#include "../creature.h"
+
+ThirstHungerEvent::ThirstHungerEvent(Date currentDate, Date timeOut, Creature *c, Scourge * scourge, int nbExecutionsToDo):
+Event(currentDate, timeOut, nbExecutionsToDo)
+{
+    this->creature = c;
+    this->scourge = scourge;
+}
+
+void ThirstHungerEvent::execute(){
+    int thirst, hunger;
+    char buff[255];  
+    
+    thirst = creature->getThirst();
+    hunger = creature->getHunger();
+    
+    thirst--; hunger--; 
+    creature->setThirst(thirst);
+    creature->setHunger(hunger); 
+                 
+    if(thirst == 5){
+        sprintf(buff, "%s is thirsty.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);            
+    }
+    else if(thirst == 3){
+        sprintf(buff, "%s is really thirsty.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);   
+    }
+    else if(thirst == 2){
+    
+        sprintf(buff, "%s is beginning to dehydrate.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f); 
+        // FIXME add state dehydrated or weak?
+        // creature->setModState(dehydrated);    
+    }
+    else if(thirst == 1){        
+        sprintf(buff, "%s is totally dehydrated.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f); 
+        // FIXME add state nearlyDead -> can't walk fast ...?
+        // creature->setModState(nearly_dead);
+    }
+    else if( thirst == 0 ){
+        sprintf(buff, "%s dies from lack of water.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);
+        scourge->creatureDeath(creature);
+        return; // no need to go further        
+    }        
+    
+    if(hunger == 5){
+        sprintf(buff, "%s is hungry.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);   
+    }    
+    else if(thirst == 3){
+        sprintf(buff, "%s is really hungry.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);   
+    }
+    else if(thirst == 2){
+    
+        sprintf(buff, "%s is starving.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f); 
+        // FIXME add state starving ? or weak?
+        // creature->setModState(starving);    
+    }
+    else if(thirst == 1){        
+        sprintf(buff, "%s is really weak.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f); 
+        // FIXME add state nearlyDead -> can't walk fast ...?
+        // creature->setModState(nearly_dead);
+    } 
+    else if( hunger == 0 ){
+        sprintf(buff, "%s dies from starvation.", creature->getName());     
+        scourge->getMap()->addDescription(buff, 1.0f, 0.5f, 0.5f);
+        scourge->creatureDeath(creature);        
+    }                  
+}
+
+ThirstHungerEvent::~ThirstHungerEvent(){
+}
