201508e2b00d4d8d40ac7d13a14b076baf1d7be7
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-07-20 01:38:30 +0000

    moved cmake stuff aside

---
+++ trunk/scourge/cmake/objects.mk
@@ -0,0 +1,18 @@
+OBJS = \
+	src/constants.o\
+	src/gui/widget.o src/gui/window.o src/gui/label.o\
+	src/gui/button.o src/gui/cardcontainer.o src/gui/scrollinglist.o\
+	src/gui/checkbox.o src/gui/multiplelabel.o src/gui/draganddrop.o\
+	src/rpg/rpgitem.o src/rpg/character.o src/rpg/monster.o\
+	src/events/event.o src/events/thirsthungerevent.o\
+	src/calendar.o src/date.o src/creature.o\
+	src/effect.o src/dungeongenerator.o src/glshape.o src/gltorch.o\
+	src/glteleporter.o\
+	src/item.o src/location.o src/main.o src/mainmenu.o src/map.o\
+	src/Md2.o src/md2shape.o src/containergui.o src/scourge.o src/sdleventhandler.o\
+	src/sdlhandler.o src/sdlscreenview.o src/shape.o src/inventory.o\
+	src/shapepalette.o src/util.o src/text.o src/gllocator.o\
+	src/minimap.o src/3ds.o src/3dsshape.o src/optionsmenu.o\
+	src/userconfiguration.o src/board.o src/battle.o src/party.o\
+	src/projectile.o
+
