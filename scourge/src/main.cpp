31f2857b0f7fa52c7b2a8034e01d0e6f306b7950
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-07-23 00:25:30 +0000

    options file now created automatically, able to run from default build location

--- scourge/src/main.cpp
+++ scourge/src/main.cpp
@@ -21,17 +21,70 @@
 
 #include "scourge.h"
 
+bool checkFile(const char *dir, const char *file) {
+  char path[300];
+  strcpy(path, dir);
+  strcat(path, file);
+  //fprintf(stderr, "\tchecking path: %s\n", path);
+  bool ret = true;
+  FILE *fp = fopen(path, "rb");
+  if(!fp || ferror(fp)) ret = false;
+  if(fp) fclose(fp);
+  return ret;
+}
+
+// this function is used to be able to run scourge while developing
+void findLocalResources(const char *appPath, char *dir) {
+  // Where are we running from?
+  strcpy(dir, appPath);	 
+  // Look in this and the parent dir for a 'data' folder
+  for(int i = 0; i < 2; i++) {
+	char *p = strrchr(dir, SEPARATOR);
+	if(!p) {
+	  dir[0] = '\0';
+	  cerr << "*** Can't find local version of data dir. You're running a distribution." << endl;
+	  return;
+	}	
+	*(p + 1) = 0;
+	//	fprintf(stderr, "*** Looking at: dir=%s\n", dir);
+	if(checkFile(dir, "data/cursor.bmp")) return;
+	// remove the last separator
+	*(p) = 0;
+  }
+}
+
 int main(int argc, char *argv[]) {
+
+  // Check to see if there's a local version of the data dir
+  // (ie. we're running in the build folder and not in a distribution)
+  char dir[300];
+  findLocalResources(argv[0], dir);
+  if(strlen(dir)) {
+	cerr << "*** Using local data dir. Not running a distribution." << endl;
+	sprintf(rootDir, "%sdata", dir);
+  }
+  
+  // config check
   if(argc >= 2 && !strcmp(argv[1], "--test-config")) {
 	cerr << "Configuration:" << endl;
 	char dir[300];
 	char file[500];
 	int dir_res = get_config_dir_name( dir, 300 );
 	int file_res = get_config_file_name( file, 500 );
-	cerr << "rootDir=" << rootDir << "\nconfigDir=" << configDir << "\nconfigFile=" << CONFIG_FILE << 
+	cerr << "starting app: " << argv[0] << endl;
+	cerr << "rootDir=" << rootDir << 
+	  "\nconfigDir=" << configDir << 
+	  "\nconfigFile=" << CONFIG_FILE << 
 	  "\ndir=" << dir << " dir_res=" << dir_res <<
 	  "\nfile=" << file << " file_res=" << file_res <<	endl;
 	exit(0);
+  }
+
+  // do a final sanity check before running the game
+  if(!checkFile(rootDir, "/cursor.bmp")) {
+	cerr << "ERROR: check for files failed in data dir: " << rootDir << endl;
+	cerr << "Either install the data files at the above location, or rebuild with ./configure --with-data-dir=<new location> or run the game from the source distribution's main directory (the one that contains src,data,etc.)" << endl;
+	exit(1);
   }
 
   new Scourge(argc, argv);
