63e40e59c5f34c3585f943952672cb9b2f46af7c
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-10-07 02:54:08 +0000

    more work on freetype text

--- scourge/src/freetype/FreeType.cpp
+++ scourge/src/freetype/FreeType.cpp
@@ -219,7 +219,7 @@ inline void pop_projection_matrix() {
 ///with freetype fonts.
 void freetype_print(const freetype_font_data &ft_font, float x, float y, const char *fmt, ...)  {
 	// We want a coordinate system where things coresponding to window pixels.
-	pushScreenCoordinateMatrix();					
+	//pushScreenCoordinateMatrix();					
 	
 	GLuint font=ft_font.list_base;
 	float h=ft_font.h/.63f;						//We make the height about 1.5* that of
@@ -236,35 +236,10 @@ void freetype_print(const freetype_font_data &ft_font, float x, float y, const c
 	va_end(ap);											// Results Are Stored In Text
 	}
 
-
-	//Here is some code to split the text that we have been
-	//given into a set of lines.
-	//This could be made much neater by using
-	//a regular expression library such as the one avliable from
-	//boost.org (I've only done it out by hand to avoid complicating
-	//this tutorial with unnecessary library dependencies).
-	const char *start_line=text;
-	vector<string> lines;
-
-	const char * c = text;;
-
-	//for(const char *c=text;*c;c++) {
-	for(;*c;c++) {
-		if(*c=='\n') {
-			string line;
-			for(const char *n=start_line;n<c;n++) line.append(1,*n);
-			lines.push_back(line);
-			start_line=c+1;
-		}
-	}
-	if(start_line) {
-		string line;
-		for(const char *n=start_line;n<c;n++) line.append(1,*n);
-		lines.push_back(line);
-	}
-
-	glPushAttrib(GL_LIST_BIT | GL_CURRENT_BIT  | GL_ENABLE_BIT | GL_TRANSFORM_BIT);	
+	//glPushAttrib(GL_LIST_BIT | GL_CURRENT_BIT  | GL_ENABLE_BIT | GL_TRANSFORM_BIT);	
+  glPushAttrib(GL_ENABLE_BIT);	
 	glMatrixMode(GL_MODELVIEW);
+  glDisable(GL_CULL_FACE);
 	glDisable(GL_LIGHTING);
 	glEnable(GL_TEXTURE_2D);
 	glDisable(GL_DEPTH_TEST);
@@ -283,34 +258,29 @@ void freetype_print(const freetype_font_data &ft_font, float x, float y, const c
 	//down by h. This is because when each character is
 	//draw it modifies the current matrix so that the next character
 	//will be drawn immediatly after it.  
-	for(unsigned int i=0;i<lines.size();i++) {
-		
-
-		glPushMatrix();
-		glLoadIdentity();
-		glTranslatef(x,y-h*i,0);
-		glMultMatrixf(modelview_matrix);
+  glPushMatrix();
+  //glLoadIdentity();
+  //glTranslatef(x,y-h*i,0);
+  glTranslatef(x, y, 0);
+  //glMultMatrixf(modelview_matrix);
+  glRotatef(180, 1, 0, 0);
 
 	//  The commented out raster position stuff can be useful if you need to
 	//  know the length of the text that you are creating.
 	//  If you decide to use it make sure to also uncomment the glBitmap command
 	//  in make_dlist().
 	//	glRasterPos2f(0,0);
-		glCallLists(lines[i].length(), GL_UNSIGNED_BYTE, lines[i].c_str());
+
+  glCallLists(strlen(text), GL_UNSIGNED_BYTE, text);
 
 	//	float rpos[4];
 	//	glGetFloatv(GL_CURRENT_RASTER_POSITION ,rpos);
 	//	float len=x-rpos[0];
 
-		glPopMatrix();
+  glPopMatrix();
+  
+  glPopAttrib();		
 
-		
-
-	}
-
-
-	glPopAttrib();		
-
-	pop_projection_matrix();
+	//pop_projection_matrix();
 }
 
