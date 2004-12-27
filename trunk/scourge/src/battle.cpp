d9f0da423c652a984903f3bb7e5ff1379b728c1d
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2004-12-27 18:55:53 +0000

    minor fixes

--- trunk/scourge/src/battle.cpp
+++ trunk/scourge/src/battle.cpp
@@ -17,8 +17,8 @@
 
 #include "battle.h"
 
-#define GOD_MODE 1
-#define MONSTER_IMORTALITY 1
+#define GOD_MODE 0
+#define MONSTER_IMORTALITY 0
 
 char *Battle::sound[] = {
   "sound/weapon-swish/handheld/sw1.wav",
