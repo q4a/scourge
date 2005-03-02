f7e465e22f18c4f67590ac997131d6dad545d29d
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2005-03-02 01:47:52 +0000

    minor change to compile w/o round

--- scourge/src/constants.h
+++ scourge/src/constants.h
@@ -111,7 +111,7 @@ using namespace std;
 #define MAX_PARTY_SIZE 4
 
 
-#define toint( x ) (int)(round( x ))
+#define toint(x) (int)(x<0 ? (x - 0.5f) : (x + 0.5f))
 
 extern char rootDir[300];
 extern char configDir[300];
