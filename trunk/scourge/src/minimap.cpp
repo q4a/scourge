af38257f23873f5cd905d330dad30fe6b968f138
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2006-01-09 00:44:38 +0000

    playing with new minimap

--- trunk/scourge/src/minimap.cpp
+++ trunk/scourge/src/minimap.cpp
@@ -25,6 +25,7 @@
 #include "item.h"
 #include "creature.h"
 #include "shapepalette.h"
+#include "rpg/rpglib.h"
 
 using namespace std;
 
@@ -75,7 +76,6 @@ MiniMap :: MiniMap(Scourge *scourge){
     mustBuildTexture = true;
 }
 
-
 MiniMap :: ~MiniMap(){
   reset();
 }
@@ -110,6 +110,229 @@ void MiniMap::reset() {
   textureInMemory = NULL;
   mustBuildTexture = true;
 }
+
+
+
+
+
+void MiniMap::prepare() {
+
+  textureSizeH = 512;
+  textureSizeW = 512;
+
+  // Create texture and copy minimap date from backbuffer on it    
+  textureInMemory = (unsigned char *) malloc( textureSizeW * textureSizeH * 4);    
+
+  glGenTextures(1, texture);    
+  glBindTexture(GL_TEXTURE_2D, texture[0]); 
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);        
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);                                          // filtre appliqué a la texture
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);  
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S, GL_CLAMP );
+  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T, GL_CLAMP ); 
+  glTexImage2D( GL_TEXTURE_2D, 0, GL_RGBA, textureSizeW, textureSizeH, 0,
+                GL_RGBA, GL_UNSIGNED_BYTE, textureInMemory );                       
+
+  // draw the entire map
+  glDisable( GL_CULL_FACE );
+  glDisable( GL_DEPTH_TEST );
+  glDisable( GL_TEXTURE_2D );
+  glEnable( GL_BLEND );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+ 
+  glPushMatrix();  
+  glLoadIdentity();
+  for( int x = 0; x < MAP_WIDTH; x++ ) {
+    for( int y = 0; y < MAP_DEPTH; y++ ) {
+      glColor3f( 0, 0, 0 );
+      glBegin( GL_QUADS );
+      glVertex2f( x, y );
+      glVertex2f( x, y + 1 );
+      glVertex2f( x + 1, y + 1 );
+      glVertex2f( x + 1, y );
+      glEnd();
+    }
+  }
+  //for( int x = MAP_OFFSET; x < MAP_WIDTH - MAP_OFFSET; x++ ) {
+//    for( int y = MAP_OFFSET; y < MAP_DEPTH - MAP_OFFSET; y++ ) {
+  for( int x = 0; x < MAP_WIDTH; x++ ) {
+    for( int y = 0; y < MAP_DEPTH; y++ ) {
+      Location *pos = scourge->getSession()->getMap()->getLocation( x, y, 0 );
+      if( pos && pos->shape && !( pos->item ) && !( pos->creature ) ) {
+        glColor3f( 1, 1, 1 );
+        glBegin( GL_QUADS );
+        glVertex2f( x, MAP_DEPTH - y );
+        glVertex2f( x, MAP_DEPTH - (y + 1) );
+        glVertex2f( x + 1, MAP_DEPTH - (y + 1) );
+        glVertex2f( x + 1, MAP_DEPTH - y );
+        glEnd();
+      }
+    }
+  }
+
+  glPopMatrix();
+
+  // Copy to a texture
+  glLoadIdentity();
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
+  glCopyTexSubImage2D(
+                     GL_TEXTURE_2D,
+                     0,      // MIPMAP level
+                     0,      // x texture offset
+                     0,      // y texture offset
+                     0,              // x window coordinates
+                     screenHeight - textureSizeH,   // y window coordinates
+                     textureSizeW,    // width
+                     textureSizeH     // height
+                     ); 
+  if(DEBUG_MINIMAP){  
+    fprintf(stderr, "OpenGl result for minimap texture building : %s\n", Util::getOpenGLError());          
+  }
+  glPopMatrix();
+  //  glPopAttrib();
+
+  glDisable( GL_BLEND );
+  glEnable( GL_CULL_FACE );
+  glEnable( GL_DEPTH_TEST );
+  glEnable( GL_TEXTURE_2D );
+}
+
+#define MINI_MAP_OFFSET 100
+#define MINI_MAP_SIZE 60
+#define MINI_MAP_BLOCK 4
+
+void MiniMap::drawMap() {
+
+  if( mustBuildTexture ) {
+    mustBuildTexture = false;
+    prepare();
+  }
+
+  int sx = scourge->getSession()->getMap()->getX() + 75 - ( MINI_MAP_SIZE / 2 );
+  int sy = scourge->getSession()->getMap()->getY() + 75 - ( MINI_MAP_SIZE / 2 );
+  int ex = sx + MINI_MAP_SIZE;
+  int ey = sy + MINI_MAP_SIZE;
+
+  glDisable( GL_CULL_FACE );
+  glDisable( GL_DEPTH_TEST );
+  glDisable( GL_TEXTURE_2D );
+  glEnable( GL_BLEND );
+  glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
+ 
+  glPushMatrix();  
+  glLoadIdentity();
+  glTranslatef( MINI_MAP_OFFSET, MINI_MAP_OFFSET, 0 );
+  glTranslatef( MINI_MAP_SIZE * MINI_MAP_BLOCK / 2, MINI_MAP_SIZE * MINI_MAP_BLOCK / 2, 0 );
+  glRotatef( scourge->getSession()->getMap()->getZRot(), 0, 0, 1 );
+  glTranslatef( -MINI_MAP_SIZE * MINI_MAP_BLOCK / 2, -MINI_MAP_SIZE * MINI_MAP_BLOCK / 2, 0 );
+
+
+  // static background
+  // Draw the minimap using a texture
+  glPushMatrix();
+//  glTranslatef( -( sx ) * MINI_MAP_BLOCK, 
+//                -( sy ) * MINI_MAP_BLOCK, 0 );
+
+  float w = MINI_MAP_SIZE * MINI_MAP_BLOCK;
+  float h = MINI_MAP_SIZE * MINI_MAP_BLOCK;
+
+  float tw = (float)(512 * w) / (float)MAP_WIDTH;
+  float th = (float)(512 * h) / (float)MAP_DEPTH;
+
+  float tx = (float)( ( sx ) * tw ) / (float)MAP_WIDTH;
+  float ty = (float)( sy * th ) / (float)MAP_DEPTH;
+  float tex = (float)( ( sx + MINI_MAP_SIZE ) * tw ) / (float)MAP_WIDTH;
+  float tey = (float)( ( sy + MINI_MAP_SIZE ) * th ) / (float)MAP_DEPTH;
+  //cerr << "w=" << w << " tx=" << tx << " h=" << h << " ty=" << ty << endl;
+
+  glEnable(GL_TEXTURE_2D);
+  glBindTexture(GL_TEXTURE_2D, texture[0]);
+  glBegin(GL_QUADS); 
+  glColor4f( 1.0f, 1.0f, 1.0f, 0.5f );
+  glTexCoord2f( tx/tw, tey/th );
+
+  glVertex2d( 0, h ); 
+  glTexCoord2f( tex/tw, tey/th );
+
+  glVertex2d( w, h ); 
+  glTexCoord2f( tex/tw, ty/th );
+
+  glVertex2d( w, 0 ); 
+  glTexCoord2f( tx/tw, ty/th );
+
+  glVertex2d( 0, 0 );     
+  glEnd();       
+  glDisable(GL_TEXTURE_2D); 
+  glPopMatrix();
+  
+  // outline
+  glColor4f( 1, 1, 1, 0.5f );
+  glBegin( GL_LINE_LOOP );
+  glVertex2f( 0, 0 );
+  glVertex2f( 0, MINI_MAP_SIZE * MINI_MAP_BLOCK );
+  glVertex2f( MINI_MAP_SIZE * MINI_MAP_BLOCK, MINI_MAP_SIZE * MINI_MAP_BLOCK );
+  glVertex2f( MINI_MAP_SIZE * MINI_MAP_BLOCK, 0 );
+  glEnd();
+
+  // naive method: draw each block
+  for( int x = sx; x < ex; x++ ) {
+    if( x < 0 || x >= MAP_WIDTH ) continue;
+    for( int y = sy; y < ey; y++ ) {
+      if( y < 0 || y >= MAP_DEPTH ) continue;
+      Location *pos = scourge->getSession()->getMap()->getLocation( x, y, 0 );
+      if( pos && ( pos->creature || pos->item ) ) {
+        if( pos->creature ) {
+          if( pos->creature->isMonster() ) {
+            if( ((Creature*)pos->creature)->getMonster()->isNpc() ) {
+              glColor4f( 1, 1, 0, 0.5f );
+            } else {
+              glColor4f( 1, 0, 0, 0.5f );
+            }
+          } else {
+            if( pos->creature == scourge->getSession()->getParty()->getPlayer() ) {
+              glColor4f( 1, 0, 1, 0.5f );
+            } else {
+              glColor4f( 0, 1, 0, 0.5f );
+            }
+          }
+        } else if( pos->item ) {
+          glColor4f( 0, 0, 1, 0.5f );
+        } else {
+          //glColor4f( 1, 1, 1, 0.5f );
+          cerr << "*** error in MiniMap::drawMap." << endl;
+        }
+        glBegin( GL_QUADS );
+        glVertex2f( ( ( x - sx ) * MINI_MAP_BLOCK ), 
+                    ( ( y - sy ) * MINI_MAP_BLOCK ) );
+        glVertex2f( ( ( x - sx ) * MINI_MAP_BLOCK ), 
+                    ( ( y - sy ) * MINI_MAP_BLOCK ) + MINI_MAP_BLOCK );
+        glVertex2f( ( ( x - sx ) * MINI_MAP_BLOCK ) + MINI_MAP_BLOCK, 
+                    ( ( y - sy ) * MINI_MAP_BLOCK ) + MINI_MAP_BLOCK );
+        glVertex2f( ( ( x - sx ) * MINI_MAP_BLOCK ) + MINI_MAP_BLOCK, 
+                    ( ( y - sy ) * MINI_MAP_BLOCK ) );
+        glEnd();
+      }
+    }
+  }
+
+  glPopMatrix();
+  glDisable( GL_BLEND );
+  glEnable( GL_CULL_FACE );
+  glEnable( GL_DEPTH_TEST );
+  glEnable( GL_TEXTURE_2D );
+}
+
+
+
+
+
+
+
+
+
+
+
 
 void MiniMap :: computeDrawValues(){        
     effectiveWidth = int(maxX - minX);
