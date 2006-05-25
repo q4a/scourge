bf0dec562307d563d4bdb0472550e5547ac912ca
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2006-05-25 16:43:45 +0000

    forgot to add file

---
+++ scourge/src/gui/confirmdialog.cpp
@@ -0,0 +1,61 @@
+/***************************************************************************
+                          confirmdialog.cpp  -  description
+                             -------------------
+    begin                : Thu Aug 28 2003
+    copyright            : (C) 2003 by Gabor Torok
+    email                : cctorok@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "confirmdialog.h"
+#include "window.h"
+#include "button.h"
+#include "label.h"
+
+#define CONFIRM_WIDTH 400
+#define CONFIRM_HEIGHT 120
+
+ConfirmDialog::ConfirmDialog( ScourgeGui *scourgeGui ) {
+  win = new Window( scourgeGui,
+										( scourgeGui->getScreenWidth() / 2 ) - ( CONFIRM_WIDTH / 2 ),
+										( scourgeGui->getScreenHeight() / 2 ) - ( CONFIRM_HEIGHT / 2 ),
+										CONFIRM_WIDTH, CONFIRM_HEIGHT,
+										"Confirmation",
+										scourgeGui->getGuiTexture(), true, 
+										Window::BASIC_WINDOW,
+										scourgeGui->getGuiTexture2() );
+  int mx = CONFIRM_WIDTH / 2;
+  okButton = new Button( mx - 80, 50, mx - 10, 80, scourgeGui->getHighlightTexture(), "Ok" );
+  win->addWidget( (Widget*)okButton );
+  cancelButton = new Button( mx + 10, 50, mx + 80, 80, scourgeGui->getHighlightTexture(), "No" );
+  win->addWidget( (Widget*)cancelButton );
+  label = new Label( 20, 20, "" );
+  win->addWidget( (Widget*)label );
+
+	mode = 0;
+}
+
+ConfirmDialog::~ConfirmDialog() {
+	delete win;
+}
+	
+void ConfirmDialog::setText( char *text ) {
+	label->setText( text );
+}
+
+void ConfirmDialog::setVisible( bool b ) {
+	win->setVisible( b );
+}
+
+bool ConfirmDialog::isVisible() {
+	return win->isVisible();
+}
+
