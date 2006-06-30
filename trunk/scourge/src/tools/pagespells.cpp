bcb031a4d8e49cf2566157e76d5f3f32e8d19c52
RhysBJ <RhysBJ@eefe38b5-e310-0410-8999-ef38e6acc92c>
2006-06-30 15:46:41 +0000

    Fixed data file constructor errors.

--- trunk/scourge/src/tools/pagespells.cpp
+++ trunk/scourge/src/tools/pagespells.cpp
@@ -1,4 +1,7 @@
 #include "pagespells.h"
+#include "dfspells.h"
+#include <wx/wx.h>
+#include "common.h"
 
 PageSpells::PageSpells()
 {
@@ -10,3 +13,33 @@ PageSpells::~PageSpells()
 	//dtor
 }
 
+void PageSpells::Init(wxNotebook *notebook, DF* dataFile)
+{
+	dfSpells = (DFSpells*)dataFile;
+	this->dataFile = dataFile;
+	page = new wxNotebookPage(notebook, ID_SpellsPage);
+
+	School *school = dfSpells->GetCurrent();
+
+	// name
+	wxStaticText *nameText = new wxStaticText(page, -1, _("Name"), wxPoint(10,10));
+	wxTextCtrl *nameEdit = new wxTextCtrl(page, -1, std2wx(school->name), wxPoint(10,30), wxSize(200,25));
+
+	notebook->AddPage(page, _("Spells"));
+}
+
+void PageSpells::UpdatePage()
+{
+}
+
+void PageSpells::GetCurrent()
+{
+}
+
+void PageSpells::SetCurrent()
+{
+}
+
+void PageSpells::ClearCurrent()
+{
+}
