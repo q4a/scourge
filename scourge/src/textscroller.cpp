a5f5b8ae0d9d5489fa7b878fd865c2f4881b6438
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2007-04-16 15:06:52 +0000

    working on new message dialog

--- scourge/src/textscroller.cpp
+++ scourge/src/textscroller.cpp
@@ -24,9 +24,12 @@ using namespace std;
 #define LINES_SHOWN 7
 #define SCROLL_SPEED 100
 #define OFFSET_DELTA 1
+#define MAX_QUEUE_SIZE 200
 
 TextScroller::TextScroller( Scourge *scourge ) {
 	this->scourge = scourge;
+	this->xp = 300;
+	this->yp = 0;
 	offset = 0;
 	lastCheck = SDL_GetTicks();
 }
@@ -36,46 +39,49 @@ TextScroller::~TextScroller() {
 
 void TextScroller::addDescription( char *description, float r, float g, float b ) {
 	string s = description;
-	// color.push_back( Color( r, g, b, 1 ) );
+	color.push_back( new Color( r, g, b, 1 ) );
 	text.insert( text.begin(), s );
+	if( text.size() == MAX_QUEUE_SIZE ) {
+		text.erase( text.end() );
+		Color *c = color[ color.size() - 1 ];
+		delete( c );
+		color.erase( color.end() );
+	}
 }
 
 void TextScroller::draw() {
 
-	Uint32 now = SDL_GetTicks();
-	if( now - lastCheck > SCROLL_SPEED ) {
-		lastCheck = now;
-		offset += OFFSET_DELTA;
-		if( offset >= LINE_HEIGHT ) {
-			offset = 0;
-			text.insert( text.begin(), "" );
+	if( scourge->getParty()->isRealTimeMode() ) {
+		Uint32 now = SDL_GetTicks();
+		if( now - lastCheck > SCROLL_SPEED ) {
+			lastCheck = now;
+			offset += OFFSET_DELTA;
+			if( offset >= LINE_HEIGHT ) {
+				offset = 0;
+				text.insert( text.begin(), "" );
+			}
 		}
 	}
 
 	glPushMatrix();
 	glLoadIdentity();
+	glTranslatef( xp, yp, 0 );
 	glDisable( GL_DEPTH_TEST );
-	int x = 50;
+	int x = 0;
 	int y = LINES_SHOWN * LINE_HEIGHT - offset;
-	int end = -1;
 	// cerr << "text.size=" << text.size() << endl;
-	for( unsigned int i = 0; i < text.size(); i++ ) {
-		if( y < 0 ) {
-			if( end == -1 ) end = i;
-		} else {
-			if( text[ i ] != "" ) {
-				scourge->getSDLHandler()->texPrint( x, y, text[ i ].c_str() );
-			}
-			y -= LINE_HEIGHT;
+	for( unsigned int i = 0; i < LINES_SHOWN && i < text.size(); i++ ) {
+		if( text[ i ] != "" ) {
+			Color *c = color[ i ];
+			float a = c->a * ( ( LINES_SHOWN - i ) / (float)LINES_SHOWN );
+			glColor4f( c->r, c->g, c->b, a  );
+			scourge->getSDLHandler()->texPrint( x, y, text[ i ].c_str() );
 		}
-	}
-	if( end > -1 ) {
-		while( (int)text.size() > end ) {
-			text.erase( text.end() );
-		}
+		y -= LINE_HEIGHT;
 	}
 	glEnable( GL_DEPTH_TEST );
 	glPopMatrix();
+	glColor4f( 1, 1, 1, 1 );
 }
 
 void TextScroller::scrollUp() {
