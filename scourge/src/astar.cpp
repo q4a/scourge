4c1441dafd7e0dbc488fd448ca378c12b735bb92
jteu004 <jteu004@eefe38b5-e310-0410-8999-ef38e6acc92c>
2007-08-02 08:02:38 +0000

    Search now uses priority queue and sets, stores pointers instead of CPathNodes. Search is n.log(n) not (n^2).log(n) now. If path to goal is not found then the path that gets closest is chosen. PCs now also take this closest path if goal cannot be reached.

--- scourge/src/astar.cpp
+++ scourge/src/astar.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
-                          util.cpp  -  description
+                          astar.cpp  -  description
                              -------------------
     begin                : Sun Jun 22 2003
     copyright            : (C) 2003 by Gabor Torok
@@ -23,7 +23,7 @@
 
 using namespace std;
 
-#define MAX_CLOSED_NODES 160
+#define MAX_CLOSED_NODES 180
 #define FAST_MAX_CLOSED_NODES 80
 
 AStar::AStar(){
@@ -32,45 +32,32 @@ AStar::AStar(){
 AStar::~AStar(){
 }
 
-// A Sample by D.S.Reynolds
-// Fixed by Jonathan Teutenberg
+// An A* implementation by Jonathan Teutenberg
 //
-// How It Works (original):
-//
-// The basic formula for the A* algorithm is f = g + h.  g represents the cost of
-// distance travelled (or gone).  h represents the estimate (or heuristic) of distance
-// from current node to the destination.  
+// An A* search is a form of best-first search, where the "goodness" of a node is determined
+// by f = g + h.  
+// Where g is the length of the path taken so far, and h, the heuristic, is an
+// estimate of the length of the rest of the path to the goal.
+// For A* to guarantee a shortest path to the goal and to remain n.log(n) complexity
+// the heuristic must never overestimate the remaining distance to the goal. 
 //
 // Data-structures:
-// The heap used for the OPEN list is fairly standard.
-// The vector for the CLOSED list is a poor choice. Membership checks are linear, but we 
-// would prefer constant, as in a hashtable. 
-// The vector for PATH is fine.
+// The priority queue for the open nodes (yet to be checked).
+// The set for the closed list - nodes we have already checked and so don't need to see again.
+// Membership checks are logarithmic for the set, but we would prefer constant, as in a hashtable. 
 //
-// Algorithm:
-//  1.  Create OPEN, CLOSED and PATH containers.
-//  2.  Start with the OPEN container containing only the starting node.
-//      Set that nodes g value to 0, h value to the maximum of the x and y distance.
-//  3.  Until the goal node is found, repeat the following:
-//        If no nodes found exit with failure.
-//        Select Node on OPEN with the lowest f value.  This is the BestNode.
-//        If BestNode is the destination, exit.
-//	  Check CLOSED to see whether we've processed BestNode before.
-//        Push BestNode to Closed (This is the only place CLOSED is populated).
-//        Determine the child nodes (adjacents) of BestNode.
-//           For each child node do the following:
-//           Set each child nodes Parent to BestNode (We'll use this later to
-//             get the path.)
-//           Calc the cost of g:  Child.g = BestNode.g + 1  (Use other than 1
-//             for various terrain)
-//           Calculate the h value using max(dx,dy). 
-//           If this is a diagonal move, add 0.5 to the h value to avoid unnecessary zig-zagging
-//           Add the child to the OPEN queue.
+// Also included is a (non-standard for A*) set for checking whether nodes are already on the open list.
+// Because we have to pop all the nodes off open at the end when we need to delete them, we expect
+// 2.n.log(n) operations (pushing and popping every child node we come across). 
+// The set now gets checked at every child insert at log(n) cost, and worst case (where no nodes are ever
+// checked twice) we can now expect 4.n.log(n) operations. 
+// However, in most cases there are 3+ children already in the open list, in which case the extra membership
+// checks cost no more. Also, since open list is smaller, we are dealing with a smaller "n" and overall it 
+// makes a speed up. Phew.
 //
-// TODO:
-// Add a goal function, so we can find a path to a location "within range" of a point.
-// Add a cost function to allow path planning over varying terrain (use the Map for this?)
-// Fix up the parent references of nodes so we can use a hash table for CLOSED
+// Because these STL containers copy on insert, we cannot store CPathNodes directly,
+// otherwise the parent pointer links between nodes would become invalid.
+//
 ///////////////////////////////////////////////////////////
 void AStar::findPath( Sint16 sx, Sint16 sy, Sint16 sz,
                      Sint16 dx, Sint16 dy, Sint16 dz,
@@ -88,170 +75,175 @@ void AStar::findPath( Sint16 sx, Sint16 sy, Sint16 sz,
 		" ignoreEndShape=" << ignoreEndShape <<
 		endl;
 
-  vector<CPathNode> OPEN;                 // STL Vectors chosen because of rapid
-  vector<CPathNode> CLOSED;               // insertions/deletions at back,
-  vector<CPathNode> PATH;                 // and Direct access to any element.
-  CPathNode Node, BestNode;               // Temporary Node and BestNode
-  bool bNodeFound = false;                // Flag if node is found in container
+  if(dx < 0 || sy < 0 || sx > MAP_WIDTH || sy > MAP_DEPTH){ //we'll waste heaps of time trying to find these
+    if(pVector->size()) pVector->erase(pVector->begin(), pVector->end()); // just clear the old path
+    return; 
+  }
+  priority_queue<CPathNode*, vector<CPathNode*>, FValueNodeComparitor> open;
+  set<CPathNode*,XYNodeComparitor> closed;   
+  vector<CPathNode> path;
 
+  set<CPathNode*,XYNodeComparitor> openContents; //to check for membership of the open queue
+  set<CPathNode*>::iterator setItr; //iterator to be used with closed when we want to delete nodes from memory
+  int closedSize = 0; //STL Set can take O(n) to determine size, so we keep a record ourselves
+  
+  CPathNode * start = new CPathNode();     // Has to be persistent, so we put it on the heap.
+  (*start).x = sx;                         // Create the start node
+  (*start).y = sy;
+  (*start).gone = 0;
+  (*start).heuristic = max(abs(dx-sx),abs(dy-sy));
+  (*start).f = (*start).gone + (*start).heuristic;    // The classic formula f = g + h
+  (*start).parent = NULL;  // no parent for the start location
+  open.push(start);            // Populate the OPEN container with the first location.
+  openContents.insert(start);
 
-  Node.x = sx;                            // Create the start node
-  Node.y = sy;
-  Node.gone = 0;
-  Node.heuristic = max(abs(dx-sx),abs(dy-sy));
-  Node.f = Node.gone + Node.heuristic;    // The classic formula f = g + h
-  Node.px = 0;                            // No parent for start location
-  Node.py = 0;                            // No parent for start location
-  OPEN.push_back(Node);                   // Populate the OPEN container with the first location
-  make_heap( OPEN.begin(), OPEN.end() );  // Create a heap from OPEN for sorting
+  CPathNode* bestNode;             //best node = lowest f-value
+  CPathNode* closestNode = start;  //closest node = lowest heuristic value
 
+  //cout << "starting search from " << sx << "," << sy << " to " << dx << "," << dy << "\n";
 
-  while (!OPEN.empty()) {
+  while (!open.empty()) {
+    bestNode = open.top();     // Set the Node with lowest f value to BESTNODE
+    open.pop();    
 
-    BestNode = OPEN.front();            // Set the Node with lowest f value to BESTNODE
-    pop_heap(OPEN.begin(), OPEN.end()); // Pop off the heap.  Actually this moves the
-                                        // far left value to the far right.  The node
-                                        // is not actually removed since the pop is to
-                                        // the heap and not the container.
-    OPEN.pop_back();                    // Remove node from right (the value we pop_heap'd)
-    
     // If at destination, break and create path below
-    if ((BestNode.x == dx) && (BestNode.y == dy)) {
-	CLOSED.push_back(BestNode);//add the goal to our path
-      break;
+    if (((*bestNode).x == dx) && ((*bestNode).y == dy)) {
+      closestNode = bestNode; 
+      closed.insert(bestNode); //add the goal to closed to ensure it gets deleted later
+      break;//build the path from closestNode back to the start
     }
 
-    // Check to see if already on CLOSED
-    bNodeFound = false;
-    for (int t=0; t<(int)CLOSED.size(); t++) {
-            if ((BestNode.x == CLOSED[t].x) &&
-                (BestNode.y == CLOSED[t].y)) {
-              bNodeFound = true;
-              break;
-            }
-     }
-    if(bNodeFound){
-	continue;
+    // Check to see if already in the closed set
+    if((setItr = closed.find(bestNode)) != closed.end()){
+      continue;
     }
 
-    CLOSED.push_back(BestNode);         // Push the BestNode onto CLOSED
-
+    if((*bestNode).heuristic < (*closestNode).heuristic)
+      closestNode = bestNode;
+    closed.insert(bestNode);         // Push the BestNode onto CLOSED
+    closedSize++;
     // Set limit to break if looking too long
-    if ( (int)CLOSED.size() > maxNodes ){
+    if ( closedSize > maxNodes ){
 	break;
     }
-
-    // Check adjacent locations (This is done in a clockwise order to lessen jaggies)
+    int x = -1;
+    int y = -1;
+    double f,g,h;
+    
     for (int i=1; i<9; i++) {
       switch(i) {
       case 1:
-        Node.x = BestNode.x;
-        Node.y = BestNode.y - 1;
+        x = (*bestNode).x;
+        y = (*bestNode).y - 1;
         break;
       case 2:
-        Node.x = BestNode.x + 1;
-        Node.y = BestNode.y - 1;
+        x = (*bestNode).x + 1;
+        y = (*bestNode).y - 1;
         break;
       case 3:
-        Node.x = BestNode.x + 1;
-        Node.y = BestNode.y;
+        x = (*bestNode).x + 1;
+        y = (*bestNode).y;
         break;
       case 4:
-        Node.x = BestNode.x + 1;
-        Node.y = BestNode.y + 1;
+        x = (*bestNode).x + 1;
+        y = (*bestNode).y + 1;
         break;
       case 5:
-        Node.x = BestNode.x;
-        Node.y = BestNode.y + 1;
+        x = (*bestNode).x;
+        y = (*bestNode).y + 1;
         break;
       case 6:
-        Node.x = BestNode.x - 1;
-        Node.y = BestNode.y + 1;
+        x = (*bestNode).x - 1;
+        y = (*bestNode).y + 1;
         break;
       case 7:
-        Node.x = BestNode.x - 1;
-        Node.y = BestNode.y;
+        x = (*bestNode).x - 1;
+        y = (*bestNode).y;
         break;
       case 8:
-        Node.x = BestNode.x - 1;
-        Node.y = BestNode.y - 1;
+        x = (*bestNode).x - 1;
+        y = (*bestNode).y - 1;
         break;
       }
 
-      if ((Node.x >= 0) && (Node.x < MAP_WIDTH) &&
-          (Node.y >= 0) && (Node.y < MAP_DEPTH)) {
+      if ((x >= 0) && (x < MAP_WIDTH) &&
+          (y >= 0) && (y < MAP_DEPTH)) {
         
         // Determine cost of distance travelled
-        if( isBlocked( Node.x, Node.y, sx, sy, dx, dy, creature, player, map, ignoreParty, ignoreEndShape ) ) {
-          Node.gone = 1000;
-        } else {
-          Node.gone = BestNode.gone + 1;
-        }
+        if( isBlocked( x, y, sx, sy, dx, dy, creature, player, map, ignoreParty, ignoreEndShape ) )
+          g = 1000;
+        else 
+          g = (*bestNode).gone + 1;
+        
         
         //heuristic calculation
-        int DX = abs(dx - Node.x);
-        int DY = abs(dy - Node.y);
+        int DX = abs(dx - x);
+        int DY = abs(dy - y);
 
 	if(i % 2 == 0) //a non-diagonal move
-		Node.heuristic = max(DX,DY)+0.5; //we discourage diagonals to remove zig-zagging
+		h = max(DX,DY)+0.5; //we discourage diagonals to remove zig-zagging
 	else
-		Node.heuristic = max(DX,DY);
-	//Node.heuristic = max(DX,DY)*2 + min(DX,DY); // the old heuristic
+		h = max(DX,DY);
 
         // The A* formula
-        Node.f = Node.gone + Node.heuristic;
-        Node.px = BestNode.x; // Point parent to last BestNode (pushed onto CLOSED)
-        Node.py = BestNode.y; // Point parent to last BestNode (pushed onto CLOSED)
+        f = g+h;
+        
+	CPathNode * next = new CPathNode();
+	(*next).x = x;
+	(*next).y = y;
+  	(*next).heuristic = h;
+	(*next).gone = g;
+	(*next).f = f;
+	(*next).parent = bestNode;
 
-          OPEN.push_back(Node);                  // Push NewNode onto OPEN
-          push_heap( OPEN.begin(), OPEN.end() ); // Push NewNode onto heap
+        // Check to see if already in the open queue by checking openContents
+        if((setItr = openContents.find(next)) != openContents.end()){
+          delete next;
+          continue;
+        }
 
-                   /*
-                   // Display OPEN and CLOSED containers (For Debugging)
-                   int i;
-                   cout << "OPEN:   ";
-                   for (i=0; i<OPEN.size(); i++)
-                   {
-                       cout << OPEN[i].x << "," << OPEN[i].y << ",";
-                       cout << OPEN[i].gone << "," << OPEN[i].heuristic << "  ";
-                   }
-                   cout << endl;
-                   cout << "CLOSED:   ";
-                   for (i=0; i<CLOSED.size(); i++)
-                   {
-                       cout << CLOSED[i].x << "," << CLOSED[i].y << ",";
-                       cout << CLOSED[i].gone << "," << CLOSED[i].heuristic << "  ";
-                   }
-                   cout << endl << endl;
-                   int ch = _getch();
-                   //*/
+        open.push(next);           // Insert into the open queue
+        openContents.insert(next);
         
       }
     }
   }
 
-  if (CLOSED.size() > 0) {
-	//cout << "got path after " << CLOSED.size() << "closed nodes.\n";
+  if (!closed.empty()) {
     // Create the path from elements of the CLOSED container
-    if(PATH.size()) PATH.erase(PATH.begin(), PATH.end());
-    PATH.push_back(CLOSED.back());
-	CLOSED.pop_back();
-    while (!CLOSED.empty()) {
-      if ((CLOSED.back().x == PATH.back().px) &&
-          (CLOSED.back().y == PATH.back().py))
-        PATH.push_back(CLOSED.back());
+    if(path.size()) path.erase(path.begin(), path.end());
 
-      CLOSED.pop_back();
+    CPathNode nextNode = *closestNode;
+    path.push_back(nextNode);
+    CPathNode * parent = nextNode.parent;
+    while(parent != NULL){
+      nextNode = *parent;
+      parent = nextNode.parent;
+      path.push_back(nextNode);
     }
 
+    for (setItr = closed.begin(); setItr != closed.end(); ++setItr){
+      bestNode = *setItr;
+      delete bestNode;
+    }
+    closed.erase(closed.begin(), closed.end());
+    
+    //and delete any left over nodes in OPEN
+    while(!open.empty()){
+        CPathNode* next = open.top();
+	open.pop();         
+        delete next;
+    }
+    //I'm unsure whether this is required to free up the memory. Better safe than sorry though.
+    openContents.erase(openContents.begin(), openContents.end()); 
 	
     // Populate the vector that was passed in by reference
     Location Fix;
     if(pVector->size()) pVector->erase(pVector->begin(), pVector->end());
-    for (int i=(PATH.size()-1); i>=0; i--) {
+    for (int i=(path.size()-1); i>=0; i--) {
       //for (container::iterator i=PATH.begin(); i!= PATH.end(); ++i)
-      Fix.x = PATH[i].x;
-      Fix.y = PATH[i].y;
+      Fix.x = path[i].x;
+      Fix.y = path[i].y;
       Fix.z = 0;
       pVector->push_back(Fix);
     }
@@ -321,17 +313,4 @@ bool AStar::isOutOfTheWay( Creature *a, vector<Location> *aPath, int aStart,
 														b->getShape()->getWidth(), 
 														b->getShape()->getDepth() );
 }
-
-///////////////////////////////////////////////////////////
-// Needed because the template doesn't know what a PathNode is
-//bool operator<(const CPathNode &a, const CPathNode &b) {
-//  return a.f < b.f;
-//}
-
-
-///////////////////////////////////////////////////////////
-// Needed because the template doesn't know what a PathNode is
-//bool operator>(const CPathNode &a, const CPathNode &b) {
-//  return a.f > b.f;
-//}
 
