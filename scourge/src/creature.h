b45f66a6808b98d3a3553cef31561f255a7d3ecd
jteu004 <jteu004@eefe38b5-e310-0410-8999-ef38e6acc92c>
2007-08-06 07:49:01 +0000

    Added Dijkstra search. Changed method of clearing paths to use Dijkstra. Changed creature calls to use new goals for search.

--- scourge/src/creature.h
+++ scourge/src/creature.h
@@ -34,6 +34,7 @@
 #include "render/renderedcreature.h"
 #include "storable.h"
 #include "rpg/rpglib.h"
+#include "astar.h"
 
 class Map;
 class Session;
@@ -295,6 +296,7 @@ class Creature : public RenderedCreature {
    * Returns true if the move is possible, false otherwise.
    */
   bool setSelXY( int x, int y, bool cancelIfNotPossible=false, int maxNodes=100 );  
+  bool setSelXY( int x, int y, GoalFunction* goal, bool cancelIfNotPossible=false, int maxNodes=100); 
   inline int getSelX() { return selX; }
   inline int getSelY() { return selY; }
   inline void setMapChanged() { mapChanged = true; }
@@ -524,7 +526,7 @@ class Creature : public RenderedCreature {
                   float *dodgePenalty,
                   bool callScript=false );
 
-   bool findPath( int x, int y, bool cancelIfNotPossible, int maxNodes, bool ignoreParty );
+   bool findPath( int x, int y, bool cancelIfNotPossible, int maxNodes, bool ignoreParty, GoalFunction* goal);
 
   /**
    * Get the position of this creature in the formation.
