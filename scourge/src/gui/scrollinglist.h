b04a671427d1e0e2fd15741a6da737db248623b4
capitol_ <capitol_@eefe38b5-e310-0410-8999-ef38e6acc92c>
2007-12-30 19:29:39 +0000

    Moved the internal backing of the scr0llerlist from char*[] to std::vector<std::string>, this enables us to simplify the creation and deletions of scrollerlist gui elements so that the gui dialog doesn't need to do any memory management of strings.

--- scourge/src/gui/scrollinglist.h
+++ scourge/src/gui/scrollinglist.h
@@ -30,97 +30,102 @@
 
 class ScrollingList : public Widget {
  protected:
-  int count;
-  const char **list;
-  const Color *colors;
-  const GLuint *icons;
-  int value;
-  int scrollerWidth, scrollerHeight;
-  int listHeight;
-  float alpha, alphaInc;
-  GLint lastTick;
-  bool inside;
-  int scrollerY;
-  bool dragging;
-  int dragX, dragY;
-  int *selectedLine;
-  int selectedLineCount;
-  DragAndDropHandler *dragAndDropHandler;
-  bool innerDrag;
-  int innerDragX, innerDragY;
-  bool highlightBorders;
-  GLuint highlight;
-  bool canGetFocusVar;
-  int lineHeight;
-  int eventType;
-  bool allowMultipleSelection;
-  int tooltipLine;
+	std::vector<std::string> list;
+	const Color *colors;
+	const GLuint *icons;
+	int value;
+	int scrollerWidth, scrollerHeight;
+	int listHeight;
+	float alpha, alphaInc;
+	GLint lastTick;
+	bool inside;
+	int scrollerY;
+	bool dragging;
+	int dragX, dragY;
+	int *selectedLine;
+	int selectedLineCount;
+	DragAndDropHandler *dragAndDropHandler;
+	bool innerDrag;
+	int innerDragX, innerDragY;
+	bool highlightBorders;
+	GLuint highlight;
+	bool canGetFocusVar;
+	int lineHeight;
+	int eventType;
+	bool allowMultipleSelection;
+	int tooltipLine;
 	bool linewrap;
 	bool iconBorder;
 
  public: 
 
-   enum {
-     EVENT_DRAG=0,
-     EVENT_ACTION
-   };
+	enum {
+		EVENT_DRAG=0,
+		EVENT_ACTION
+	};
 
-   bool debug;
+	bool debug;
 
-  ScrollingList(int x, int y, int w, int h, GLuint highlight, DragAndDropHandler *dragAndDropHandler = NULL, int lineHeight=15);
-  virtual ~ScrollingList();
+	ScrollingList(int x, int y, int w, int h, GLuint highlight, DragAndDropHandler *dragAndDropHandler = NULL, int lineHeight=15);
+	virtual ~ScrollingList();
 
 	inline void setTextLinewrap( bool b ) { linewrap = b; }
 	inline void setIconBorder( bool b ) { iconBorder = b; }
 
-  inline void setAllowMultipleSelection( bool b ) { allowMultipleSelection = b; }
-  inline bool getAllowMultipleSelection() { return allowMultipleSelection; }
-  inline int getLineCount() { return count; }
-  void setLines(int count, const char *s[], const Color *colors=NULL, const GLuint *icon=NULL);
-  inline const char *getLine(int index) { return list[index]; }
+	inline void setAllowMultipleSelection( bool b ) { allowMultipleSelection = b; }
+	inline bool getAllowMultipleSelection() { return allowMultipleSelection; }
+	inline int getLineCount() { return list.size(); }
+	void setLines(int count, const char *s[], const Color *colors=NULL, const GLuint *icon=NULL);
+	inline const char *getLine(int index) { return list[index].c_str(); }
 
-  inline int getSelectedLine() { return ( selectedLine ? selectedLine[ 0 ] : -1 ); }
-  void setSelectedLine( int n );
-  inline bool isSelected( int line ) { 
-    if( !selectedLine ) {
-      return false; 
-    } else {
-      for( int i = 0; i < selectedLineCount; i++ ) {
-        if( selectedLine[i] == line ) return true; 
-      }
-    }
-    return false; 
-  }
-  inline int getSelectedLineCount() { return selectedLineCount; }
-  inline int getSelectedLine( int index ) { return selectedLine[ index ]; }
+	inline int getSelectedLine() { return ( selectedLine ? selectedLine[ 0 ] : -1 ); }
+	void setSelectedLine( int n );
+	inline bool isSelected( int line ) { 
+		if( !selectedLine ) {
+			return false; 
+		} else {
+			for( int i = 0; i < selectedLineCount; i++ ) {
+				if( selectedLine[i] == line ) return true; 
+			}
+		}
+		return false; 
+	}
+	inline int getSelectedLineCount() { return selectedLineCount; }
+	inline int getSelectedLine( int index ) { return selectedLine[ index ]; }
 
-  void drawWidget(Widget *parent);
+	void drawWidget(Widget *parent);
 
-  inline int getEventType() { return eventType; }
+	inline int getEventType() { return eventType; }
 
-  /**
-	 Return true, if the event activated this widget. (For example, button push, etc.)
-	 Another way to think about it is that if true, the widget fires an "activated" event
-	 to the outside world.
-   */
-  bool handleEvent(Widget *parent, SDL_Event *event, int x, int y);
+	/**
+	Return true, if the event activated this widget. (For example, button push, etc.)
+	Another way to think about it is that if true, the widget fires an "activated" event
+	to the outside world.
+	*/
+	bool handleEvent(Widget *parent, SDL_Event *event, int x, int y);
 
-  void removeEffects(Widget *parent);
+	void removeEffects(Widget *parent);
 
-  // don't play sound when the value changes
-  virtual inline bool hasSound() { return false; }
+	// don't play sound when the value changes
+	virtual inline bool hasSound() { return false; }
 
-  inline bool canGetFocus() { return canGetFocusVar; }
-  inline void setCanGetFocus(bool b) { this->canGetFocusVar = b; }
+	inline bool canGetFocus() { return canGetFocusVar; }
+	inline void setCanGetFocus(bool b) { this->canGetFocusVar = b; }
 
-  int getLineAtPoint( int x, int y );
+	int getLineAtPoint( int x, int y );
 
  private:
-  void selectLine(int x, int y, bool addToSelection = false, bool mouseDown=false );
-  void drawIcon( int x, int y, GLuint icon, Widget *parent );  
+	void selectLine(int x, int y, bool addToSelection = false, bool mouseDown=false );
+	void drawIcon( int x, int y, GLuint icon, Widget *parent );  
 	void moveSelectionUp();
 	void moveSelectionDown();
-	char *printLine( Widget *parent, int x, int y, char *s );
+
+	/**
+	* Prints s
+	* if linewrap is true it breaks the line so that no line is longer that getWidth()
+	* else it just prints it
+	*/
+	void printLine( Widget *parent, int x, int y, const std::string& s );
 };
 
 #endif
