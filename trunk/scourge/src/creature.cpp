ea39987c0115e4dbb7eb76b63854cce541c12e48
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2008-04-29 05:17:31 +0000

    map editor now runs again

--- trunk/scourge/src/creature.cpp
+++ trunk/scourge/src/creature.cpp
@@ -982,11 +982,11 @@ Item *Creature::removeInventory(int index) {
 
     // remove it from quickspell slot
     for( int i = 0; i < 12; i++ ) {
-      Storable *storable = session->getParty()->getPlayer()->getQuickSpell( i );
+      Storable *storable = getQuickSpell( i );
         if( storable ) {
           if( storable->getStorableType() == Storable::ITEM_STORABLE ) {
             if ( (Item*)storable == item ) {
-              session->getParty()->getPlayer()->setQuickSpell( i, NULL );
+              setQuickSpell( i, NULL );
             }
           }
         }
