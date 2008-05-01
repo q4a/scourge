ed38b0b1f598679cb3838e70c16cfd51014e6cee
lordtoran <lordtoran@eefe38b5-e310-0410-8999-ef38e6acc92c>
2008-05-01 13:36:23 +0000

    Make configure/translate.sh create POTFILES.in automagically so translators have one thing less to worry about. It digged out a lot of new gettext strings by the way.

--- scourge/translate.sh
+++ scourge/translate.sh
@@ -2,6 +2,11 @@
 srcdir=.
 topdir=$srcdir
 domain=scourge
+
+echo 'Creating po/POTFILES.in ...'
+
+ ( find src -name '*.cpp' -print && find src -name '*.h' -print && find ../scourge_data/config -name '*.cfg' -print && find ../scourge_data/maps -name '*.cfg' -print && find ../scourge_data/script -name '*.nut' -print ) > po/POTFILES.in
+
 pushd po
 make update-po
 popd
