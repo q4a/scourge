866f100044a431b54fb7b70e5e42fe366a81cf9d
lordtoran <lordtoran@eefe38b5-e310-0410-8999-ef38e6acc92c>
2008-05-13 18:38:18 +0000

    Create function skeletons in the Cutscene class.

--- trunk/scourge/src/render/cutscene.h
+++ trunk/scourge/src/render/cutscene.h
@@ -25,9 +25,60 @@ class Session;
 class Cutscene {
 private:
   Session *session;
+
+  Uint32 cameraStartTime, cameraDuration;
+  Uint32 dialogLineStartTime, dialogLineDuration;
+  Uint32 doNothingStartTime, doNothingDuration;
+  Uint32 letterboxStartTime, letterboxEndTime;
+
+  int letterboxHeight, currentLetterboxHeight;
+
+  void startLetterbox();
+  void endLetterbox();
+
+  // camera state
+  float originalX, fromX, currentX, toX;
+  float originalY, fromY, currentY, toY;
+  float originalZ, fromZ, currentZ, toZ;
+
+  float originalXRot, fromXRot, currentXRot, toXRot;
+  float originalYRot, fromYRot, currentYRot, toYRot;
+  float originalZRot, fromZRot, currentZRot, toZRot;
+
+  float originalZoom, fromZoom, currentZoom, toZoom;
+
+  bool cutscenePlaying, cutsceneFinished, endingCutscene;
+
 public:
   Cutscene( Session *session );
   ~Cutscene();
+
+  // scene setup
+  void startCutsceneMode();
+  void placeActor( Creature *actor, int x, int y, int facingDirection );
+  void placeCamera( float x, float y, float z, float xRot, float yRot, float zRot, float zoom );
+  void endCutsceneMode();
+
+  // choreography
+  void animateCamera( float targetX, float targetY, float targetZ, float targetXRot, float targetYRot, float targetZRot, float targetZoom, Uint32 duration );
+  void startDialogLine( Creature *actor, std::string *text, Uint32 duration ); //TODO: requires a "floating text" widget that takes a creature as a parameter
+  void doNothing( Uint32 duration );
+
+  // cuts
+  void fade( Uint32 duration );
+
+  // scene information
+  void updateCutscene(); // update all information about the scene
+  inline int getCurrentLetterboxHeight() { return currentLetterboxHeight; }
+  inline bool isCutscenePlaying() { return cutscenePlaying; }
+  inline bool isCutsceneFinished() { return cutsceneFinished; }
+  inline float getCurrentX() { return currentX; }
+  inline float getCurrentY() { return currentY; }
+  inline float getCurrentZ() { return currentZ; }
+  inline float getCurrentXRot() { return currentXRot; }
+  inline float getCurrentYRot() { return currentYRot; }
+  inline float getCurrentZRot() { return currentZRot; }
+  inline float getCurrentZoom() { return currentZoom; }
 };
 
 #endif
