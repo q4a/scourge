daf5c812a1bdca0fb641ab5a8b59aa31adf42c5c
lordtoran <lordtoran@eefe38b5-e310-0410-8999-ef38e6acc92c>
2009-05-08 15:18:39 +0000

    (Mostly) fixing HQ bridge by making roads not pave water. It would be nice to have such bridges generated automagically over water :-)

--- trunk/scourge/src/sqbinding/sqmission.h
+++ trunk/scourge/src/sqbinding/sqmission.h
@@ -80,6 +80,7 @@ public:
 	static int _removeMapEffect( HSQUIRRELVM vm );
 	static int _removeMapPosition( HSQUIRRELVM vm );
 	static int _flattenChunk( HSQUIRRELVM vm );
+	static int _flattenLandChunk( HSQUIRRELVM vm );
 	static int _getShape( HSQUIRRELVM vm );
 
 	static int _getHeightMap( HSQUIRRELVM vm );
