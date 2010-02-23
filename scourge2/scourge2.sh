f4f858b7d889ff6f2ba874dfb0d107fd67a79387
gabortorok <gabortorok@eefe38b5-e310-0410-8999-ef38e6acc92c>
2010-02-23 16:27:49 +0000

    slight improvement to start script

--- scourge2/scourge2.sh
+++ scourge2/scourge2.sh
@@ -1,5 +1,11 @@
-PLATFORM=macosx
-JAVA=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands/java
+JAVA=java
+OS=`uname`
+case "${OS}" in
+	Linux) PLATFORM=linux;;
+	Darwin) PLATFORM=macosx; JAVA=/System/Library/Frameworks/JavaVM.framework/Versions/1.6.0/Commands/java;;
+	Solaris) PLATFORM=solaris;;
+	*) PLATFORM=windows;;
+esac	
 
 CP=out/artifacts/scourge2/scourge2.jar
 for jar in lib/*.jar; do
